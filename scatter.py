2df5c55f193208b4ad2b48b80385ed99db2ee6b5
Pai-Sho <jackhessburg94@gmail.com>
2019-11-19 21:23:21 -0700

    Added legend

--- scatter.py
+++ scatter.py
@@ -2,6 +2,7 @@ import pandas as pd
 from bokeh.plotting import figure, output_file, show, ColumnDataSource
 from bokeh.transform import factor_cmap
 from bokeh.palettes import Spectral6
+from bokeh.models import Legend
 
 # Declare output output output file
 output_file("scatter.html")
@@ -16,9 +17,9 @@ test_frame = data_frame
 style = test_frame['Style']
 
 source = ColumnDataSource(data=dict(
+    style=style,
     x=test_frame['Year'],
     y=test_frame['Mean rating'],
-    style=style,
     titles=test_frame['Title'],
     artists=test_frame['Artist'],
     imgs=test_frame['Image URL'],
@@ -49,12 +50,11 @@ TOOLTIPS = """
 
 colors = factor_cmap('style', palette=Spectral6, factors=style.unique())
 
-
-
-p = figure(plot_width=1500, plot_height=800, tooltips=TOOLTIPS,
+p = figure(plot_width=1500, plot_height=1000, tooltips=TOOLTIPS,
            title="Test graph please ignore", lod_threshold=None)
 
-p.circle('x', 'y', size=6, source=source, fill_color=colors, line_color=colors)
+p.circle('x', 'y', size=6, source=source, fill_color=colors, line_color=colors, legend=colors)
+
 
 p.xaxis.axis_label = 'Year'
 p.yaxis.axis_label = 'Mean Rating'
@@ -63,7 +63,7 @@ p.yaxis.ticker = [-3,-2,-1,0,1,2,3]
 p.yaxis.major_label_overrides = {   3: 'I like it a lot',
                                     2: 'I like it',
                                     1: 'I like it somewhat',
-                                    0: 'I am Neutral',
+                                    0: 'I am neutral',
                                     -1: 'I dislike it somewhat',
                                     -2: 'I dislike it',
                                     -3: 'I dislike it a lot',}
@@ -72,5 +72,7 @@ p.min_border_left = 100
 p.min_border_right = 100
 p.min_border_top = 200
 p.min_border_bottom = 200
+
+p.legend.location = "bottom_left"
 
 show(p)
