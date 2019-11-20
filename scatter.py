7015f1ce004d1aad9e42d20987acc5f838ffb56f
Pai-Sho <jackhessburg94@gmail.com>
2019-11-20 15:31:57 -0700

    Added tabs to color plot based on style or body parts

--- scatter.py
+++ scatter.py
@@ -3,6 +3,8 @@ from bokeh.plotting import figure, output_file, show, ColumnDataSource
 from bokeh.transform import factor_cmap
 from bokeh.palettes import Spectral6
 from bokeh.models import Legend
+from bokeh.models.widgets import RadioGroup
+from bokeh.models.widgets import Panel, Tabs
 
 # Declare output output output file
 output_file("scatter.html")
@@ -15,6 +17,7 @@ data_frame = pd.read_csv('WikiArtClean.csv',encoding='utf_8')
 test_frame = data_frame
 
 style = test_frame['Style']
+bodyparts = test_frame['Face or body']
 
 source = ColumnDataSource(data=dict(
     style=style,
@@ -22,6 +25,7 @@ source = ColumnDataSource(data=dict(
     y=test_frame['Mean rating'],
     titles=test_frame['Title'],
     artists=test_frame['Artist'],
+    bodyparts=test_frame['Face or body'],
     imgs=test_frame['Image URL'],
 ))
 
@@ -48,19 +52,19 @@ TOOLTIPS = """
 </div>
 """
 
-colors = factor_cmap('style', palette=Spectral6, factors=style.unique())
+colors1 = factor_cmap('style', palette=Spectral6, factors=style.unique())
 
-p = figure(plot_width=1500, plot_height=1000, tooltips=TOOLTIPS,
+p1 = figure(plot_width=1500, plot_height=1000, tooltips=TOOLTIPS,
            title="Test graph please ignore", lod_threshold=None)
 
-p.circle('x', 'y', size=6, source=source, fill_color=colors, line_color=colors, legend=colors)
+p1.circle('x', 'y', size=6, source=source, fill_color=colors1, line_color=colors1, legend=colors1)
 
 
-p.xaxis.axis_label = 'Year'
-p.yaxis.axis_label = 'Mean Rating'
+p1.xaxis.axis_label = 'Year'
+p1.yaxis.axis_label = 'Mean Rating'
 
-p.yaxis.ticker = [-3,-2,-1,0,1,2,3]
-p.yaxis.major_label_overrides = {   3: 'I like it a lot',
+p1.yaxis.ticker = [-3,-2,-1,0,1,2,3]
+p1.yaxis.major_label_overrides = {   3: 'I like it a lot',
                                     2: 'I like it',
                                     1: 'I like it somewhat',
                                     0: 'I am neutral',
@@ -68,11 +72,46 @@ p.yaxis.major_label_overrides = {   3: 'I like it a lot',
                                     -2: 'I dislike it',
                                     -3: 'I dislike it a lot',}
 
-p.min_border_left = 100
-p.min_border_right = 100
-p.min_border_top = 200
-p.min_border_bottom = 200
+p1.min_border_left = 100
+p1.min_border_right = 100
+p1.min_border_top = 200
+p1.min_border_bottom = 200
 
-p.legend.location = "bottom_left"
+p1.legend.location = "bottom_left"
 
-show(p)
+tab1 = Panel(child=p1, title="style")
+
+
+
+colors2 = factor_cmap('bodyparts', palette=Spectral6, factors=bodyparts.unique())
+
+p2 = figure(plot_width=1500, plot_height=1000, tooltips=TOOLTIPS,
+           title="Test graph please ignore", lod_threshold=None)
+
+p2.circle('x', 'y', size=6, source=source, fill_color=colors2, line_color=colors2, legend=colors2)
+
+
+p2.xaxis.axis_label = 'Year'
+p2.yaxis.axis_label = 'Mean Rating'
+
+p2.yaxis.ticker = [-3,-2,-1,0,1,2,3]
+p2.yaxis.major_label_overrides = {   3: 'I like it a lot',
+                                    2: 'I like it',
+                                    1: 'I like it somewhat',
+                                    0: 'I am neutral',
+                                    -1: 'I dislike it somewhat',
+                                    -2: 'I dislike it',
+                                    -3: 'I dislike it a lot',}
+
+p2.min_border_left = 100
+p2.min_border_right = 100
+p2.min_border_top = 200
+p2.min_border_bottom = 200
+
+p2.legend.location = "bottom_left"
+
+tab2 = Panel(child=p2, title="bodyparts")
+
+tabs = Tabs(tabs=[ tab1, tab2 ])
+
+show(tabs)
